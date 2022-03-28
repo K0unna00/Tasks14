e851d6f6d2e8e7d90197b6dee0890092f2124da8
K0unna00 <78pf4jz@code.edu.az>
2022-03-28 23:04:32 +0400

    Tasks14

---
+++ Tasks14/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tasks14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Adini daxil edin");
+            string username = Console.ReadLine();
+            Console.WriteLine("User Passwordunu daxil edin");
+            string password = Console.ReadLine();
+            User user1 = new User(username)
+            {
+                Password = password
+            };
+            user1.GetInfo();
+            string testStr = "Salam salam S4l4m";
+            Console.WriteLine(user1.HasLower(testStr));
+            Console.WriteLine(user1.HasUpper(testStr));
+            Console.WriteLine(user1.HasDigit(testStr));
+        }
+    }
+}
---
+++ Tasks14/User.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks14
+{
+    internal class User
+    {
+        public User(string name)
+        {
+            this._name = name;
+        }
+
+
+
+        private string _name = "No Name";
+        private string _password = "No Password";
+
+
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length >= 6 && value.Length <= 25)
+                {
+                    _name = value;
+                }
+            }
+
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                if (value.Length >= 8 && value.Length <= 25)
+                {
+                    bool[] boolarr = { false, false, false };
+                    foreach (var item in value)
+                    {
+                        if (char.IsDigit(item))
+                        {
+                            boolarr[0] = true;
+                        }
+                        else if (char.IsLower(item))
+                        {
+                            boolarr[0] = true;
+                        }
+                        else if (char.IsUpper(item))
+                        {
+                            boolarr[0] = true;
+                        }
+                        if (boolarr[0] && boolarr[1] && boolarr[2])
+                        {
+                            Console.WriteLine("Isledi");
+                            _password = value;
+                            break;
+                        }
+                    }
+                    
+
+                }
+            }
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine(_name + " " + _password);
+        }
+        public bool HasDigit(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsDigit(item)) return true;
+            }
+            return false;
+        }
+        public bool HasUpper(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsUpper(item)) return true;
+            }
+            return false;
+        }
+        public bool HasLower(string str)
+        {
+            foreach (var item in str)
+            {
+                if (char.IsLower(item)) return true;
+            }
+            return false;
+        }
+    }
+}
